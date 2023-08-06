02ba623eeea12528914fed236f85dfd9b3c7cd99
Anthony Silva <anthony.silvac@gmail.com>
2023-08-06 01:13:27 -0400

    se agrega seccion servicios y ubicacion

--- root/js/scripts.js
+++ root/js/scripts.js
@@ -1,36 +1,36 @@
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const servicios = document.querySelectorAll('.servContainter');
+  // document.addEventListener('DOMContentLoaded', function () {
+  //   const servicios = document.querySelectorAll('.serv');
 
-    servicios.forEach((servicio) => {
-      const servText = servicio.querySelector('.servText');
-      const servInfo = servicio.querySelector('.servInfo');
+  //   servicios.forEach((servicio) => {
+  //     const servText = servicio.querySelector('.serv-Text');
+  //     const servInfo = servicio.querySelector('.serv-info');
 
-      servText.addEventListener('click', function (event) {
-        // Ocultar todos los servicios, excepto el que se ha clickeado
-        servicios.forEach((serv) => {
-          if (serv !== servicio) {
-            serv.style.display = 'none';
-          }
-        });
+  //     servText.addEventListener('click', function (event) {
+  //       // Ocultar todos los servicios, excepto el que se ha clickeado
+  //       servicios.forEach((serv) => {
+  //         if (serv !== servicio) {
+  //           serv.style.display = 'none';
+  //         }
+  //       });
 
-        // Mostrar el contenido adicional del servicio clickeado
-        servInfo.style.display = 'inline-block';
+  //       // Mostrar el contenido adicional del servicio clickeado
+  //       servInfo.style.display = 'inline-block';
 
-        event.stopPropagation(); // Evitar que el clic se propague al documento
-      });
-    });
+  //       event.stopPropagation(); // Evitar que el clic se propague al documento
+  //     });
+  //   });
 
-    // Agregar un evento clic en el documento para ocultar la información adicional al hacer clic fuera de los servicios
-    document.addEventListener('click', function () {
-      // Mostrar todos los servicios y ocultar la información adicional
-      servicios.forEach((serv) => {
-        serv.style.display = 'flex';
-        const info = serv.querySelector('.servInfo');
-        info.style.display = 'none';
-      });
-    });
-  });
+  //   // Agregar un evento clic en el documento para ocultar la información adicional al hacer clic fuera de los servicios
+  //   document.addEventListener('click', function () {
+  //     // Mostrar todos los servicios y ocultar la información adicional
+  //     servicios.forEach((serv) => {
+  //       serv.style.display = 'flex';
+  //       const info = serv.querySelector('.serv-info');
+  //       info.style.display = 'none';
+  //     });
+  //   });
+  // });
 
 
   
