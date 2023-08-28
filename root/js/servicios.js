3e5a8d04ad185b1d951d0d417aee3300758f698a
Anthony Silva <anthony.silvac@gmail.com>
2023-08-28 17:09:20 -0400

    pruebas js 1024

--- root/js/servicios.js
+++ root/js/servicios.js
@@ -1,69 +1,44 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   const servicios = document.querySelectorAll('.serv');
-//   const isAbove1024 = () => window.innerWidth > 1024;
+// Obtén todos los elementos con la clase 'serv'
+const servicios = document.querySelectorAll('.serv');
 
-//   // Función para ocultar un servicio
-//   function hideService(servicio) {
-//     servicio.style.display = 'none';
-//     const servText = servicio.querySelector('.serv-text');
-//     servText.style.display = 'block'; // Mostrar el h2 nuevamente
-//     const servInfo = servicio.querySelector('.serv-info');
-//     servInfo.style.display = 'none'; // Ocultar la información adicional
-//   }
+// Agrega un evento clic a cada elemento 'serv'
+servicios.forEach((servicio) => {
+    const servText = servicio.querySelector('.serv-text');
+    const servInfo = servicio.querySelector('.serv-info');
+    const servImg = servicio.querySelector('.serv-img');
 
-//   // Función para mostrar un servicio
-//   function showService(servicio) {
-//     servicio.style.display = 'flex';
-//     servicio.style.justifyContent = 'center';
-//     servicio.style.alignItems = 'center';
-//     const servText = servicio.querySelector('.serv-text');
-//     servText.style.display = 'none'; // Ocultar el h2
-//     const servInfo = servicio.querySelector('.serv-info');
-//     servInfo.style.display = 'inline-block'; // Mostrar información adicional
-//   }
+    servText.addEventListener('click', () => {
+        // Oculta todos los elementos 'serv' excepto el seleccionado
+        servicios.forEach((s) => {
+            if (s !== servicio) {
+                s.style.display = 'none';
+            }
+        });
 
-//   servicios.forEach((servicio) => {
-//     const servText = servicio.querySelector('.serv-text');
-//     const servInfo = servicio.querySelector('.serv-info');
+        // Muestra el 'serv' seleccionado
+        servicio.style.display = 'block';
 
-//     servText.addEventListener('click', function (event) {
-//       if (isAbove1024()) {
-//         servicios.forEach((serv) => {
-//           if (serv !== servicio) {
-//             hideService(serv);
-//           }
-//         });
+        // Muestra el 'serv-info' correspondiente
+        servInfo.classList.remove('ocultar');
+        servInfo.classList.add('mostrar');
+        servImg.classList.remove('ocultar');
+        servImg.classList.add('mostrar');
+    });
 
-//         showService(servicio);
+    // Agrega un evento clic al botón dentro de 'serv-info'
+    const btnServInfo = servInfo.querySelector('.btn');
+    btnServInfo.addEventListener('click', (e) => {
+        e.stopPropagation(); // Evita que el clic se propague a 'serv-text'
 
-//         event.stopPropagation(); // Evitar que el clic se propague al documento
-//       }
-//     });
+        // Oculta el 'serv-info' actual
+        servInfo.classList.remove('mostrar');
+        servInfo.classList.add('ocultar');
+        servImg.classList.remove('ocultar');
+        servImg.classList.add('mostrar');
 
-//     // Agregar un evento clic al h3 categorías para mostrar todos los servicios y ocultar la información adicional
-//     const categoria = servInfo.querySelector('h3');
-//     categoria.addEventListener('click', function (event) {
-//       if (isAbove1024()) {
-//         servicios.forEach((serv) => {
-//           showService(serv);
-//         });
-
-//         event.stopPropagation(); // Evitar que el clic se propague al documento
-//       }
-//     });
-//   });
-
-//   // Agregar un evento clic al documento para ocultar la información adicional al hacer clic fuera de los servicios
-//   document.addEventListener('click', function (event) {
-//     if (isAbove1024()) {
-//       const target = event.target;
-//       const isServClicked = Array.from(servicios).some((servicio) => servicio.contains(target));
-
-//       if (!isServClicked) {
-//         servicios.forEach((serv) => {
-//           showService(serv);
-//         });
-//       }
-//     }
-//   });
-// });
+        // Muestra todos los elementos 'serv'
+        servicios.forEach((s) => {
+            s.style.display = 'block';
+        });
+    });
+});
